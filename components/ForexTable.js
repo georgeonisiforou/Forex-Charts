fd052ab8d58ff86bca583e30c8d592b8bd22d462
George.O <george.o@tiomarkets.com>
2023-09-04 14:44:23 +0300

    fully dynamic graph

--- components/ForexTable.js
+++ components/ForexTable.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { styled } from "styled-components";
 import { useState } from "react";
 
@@ -10,6 +10,18 @@ const Container = styled.div`
   align-items: center;
   padding: 64px;
   gap: 32px;
+`;
+
+const Graph = styled.div`
+  display: flex;
+  gap: 16px;
+`;
+
+const InfoSidebar = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  width: 200px;
 `;
 
 const Table = styled.div`
@@ -39,19 +51,25 @@ const FetchDataBtn = styled.button`
   height: 50px;
   background-color: aliceblue;
   cursor: pointer;
-  border-radius: 15px;
-  border: none;
-  box-shadow: 0px 0px 10px 2px black;
+  border-radius: 5px;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  box-shadow: 0px 0px 10px 2px rgba(0, 0, 0, 0.1);
+  color: blueviolet;
+  font-size: 16px;
+  font-weight: 600;
 `;
 
 const InputFile = styled.input`
   border: 1px solid black;
+  width: 350px;
+  border-radius: 8px;
+  box-shadow: 0px 0px 10px 1px rgba(0, 0, 0, 0.4);
 `;
 
 const GraphContainer = styled.div`
   width: 1200px;
   height: 450px;
-  background-color: rgba(0, 0, 0, 0.05);
+  background-color: rgba(0, 0, 0, 0.8);
   position: relative;
   border-bottom: 1px solid black;
   border-left: 1px solid black;
@@ -84,11 +102,10 @@ const XAxisValue = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  /* align-items: center; */
   position: absolute;
   left: ${({ positionl }) => `${positionl}px`};
   bottom: -21px;
-  font-size: 10px;
+  font-size: 9px;
 `;
 
 const LowHighLine = styled.div`
@@ -97,7 +114,7 @@ const LowHighLine = styled.div`
   bottom: ${({ positionb }) => `${positionb}px`};
   left: ${({ positionl }) => `${positionl}px`};
   height: ${({ lineheight }) => `${lineheight}px`};
-  background-color: gray;
+  background-color: white;
 `;
 
 const OpenCloseBar = styled.div`
@@ -107,19 +124,32 @@ const OpenCloseBar = styled.div`
   bottom: ${({ positionb }) => `${positionb}px`};
   left: ${({ positionl }) => `${positionl}px`};
   height: ${({ barheight }) => `${barheight}px`};
+  opacity: 0.5;
 `;
 
 const ForexTable = () => {
   const [fileDoc, setFileDoc] = useState([]);
   const [usableData, setUsableData] = useState([]);
-  const [chart, setChart] = useState([]);
+  const [compilation, setCompilation] = useState([]);
+  const [xAxis, setXAxis] = useState([]);
+  const [pretty, setPretty] = useState([]);
+  const [xAxisV, setXAxisV] = useState([]);
+  const [valuesAndPoints, setValuesAndPoints] = useState([]);
+  const [lowHighDifferenceValue, setLowHighDifferenceValue] = useState(0);
+  const [lowest, setLowest] = useState(0);
+  const [xAxisPointsDistance, setXAxisPointsDistance] = useState(0);
+  const [graph, setGraph] = useState([]);
+  const [barInfo, setBarInfo] = useState({});
+  const [selectedBar, setSelectedBar] = useState();
+
+  const ref = useRef(0);
 
   const convertText = () => {
     let arrayFromText = fileDoc.split(/\r?\n/);
 
-    let data = arrayFromText.map((item, id) => item.split(","));
+    let data = arrayFromText.map((item, _) => item.split(","));
 
-    let dataObjects = data.map((data, id) => {
+    let dataObjects = data.map((data, _) => {
       let keys = ["date", "time", "open", "high", "low", "close", "volume"];
 
       const obj = Object.fromEntries(
@@ -130,11 +160,162 @@ const ForexTable = () => {
     });
 
     const chartData = dataObjects.filter((element) => element.time === "18:00");
-    setChart(chartData);
-
-    // const slicedArray = dataObjects.slice(0, 50);
 
     setUsableData(dataObjects);
+
+    let startingDate = dataObjects[0].date;
+
+    let currentDate = startingDate;
+    let low = 100000;
+    let high = 0;
+    let openDayPrice = dataObjects[0].open;
+    let dayArray = [];
+    let compilationArray = [];
+
+    dataObjects.map((el, idx) => {
+      if (el.date === currentDate) {
+        low = Math.min(low, el.low);
+        high = Math.max(high, el.high);
+      } else {
+        compilationArray.push({
+          date: currentDate,
+          low: low,
+          high: high,
+          close: dataObjects[idx - 1].close,
+          open: openDayPrice,
+        });
+        currentDate = el.date;
+        low = 100000;
+        high = 0;
+        dayArray = [];
+        openDayPrice = dataObjects[idx].open;
+      }
+      dayArray.push({ date: currentDate, low: low, high: high });
+    });
+
+    console.log("compilation", compilationArray);
+    setCompilation(compilationArray);
+
+    //'///////
+
+    let lowestPrice = 1000000;
+    let highestPrice = 0;
+    compilationArray.map((el, idx) => {
+      lowestPrice = Math.min(lowestPrice, el.low);
+      highestPrice = Math.max(highestPrice, el.high);
+    });
+
+    setLowest(lowestPrice);
+
+    let lowHighDifference = highestPrice - lowestPrice;
+
+    setLowHighDifferenceValue(lowHighDifference);
+
+    let yAxisPointsNumber = 10;
+
+    let yAxisPointsStep = lowHighDifference / yAxisPointsNumber;
+
+    const lowHighLineHeightInPixels = (low, high) => {
+      let result;
+      if (low > high) {
+        result = (low - high) / (lowHighDifference / 450);
+      } else {
+        result = (high - low) / (lowHighDifference / 450);
+      }
+
+      return result;
+    };
+
+    const openCloseHeightInPixels = (open, close) => {
+      let result;
+      if (open > close) {
+        result = (open - close) / (lowHighDifference / 450);
+      } else {
+        result = (close - open) / (lowHighDifference / 450);
+      }
+
+      return result;
+    };
+
+    ////
+    let graphElementsInPixelsArray = [];
+
+    compilationArray.map((el, idx) => {
+      graphElementsInPixelsArray[idx] = {
+        lowHighLineHeightPixels: Number(
+          lowHighLineHeightInPixels(el.low, el.high).toFixed(5)
+        ),
+        openCloseHeightPixels: Math.abs(
+          Number(openCloseHeightInPixels(el.open, el.close).toFixed(5))
+        ),
+        ...el,
+      };
+    });
+
+    setGraph(graphElementsInPixelsArray);
+
+    let xAxisPointsNum = graphElementsInPixelsArray.length;
+
+    let xAxisPointsDistanceInPixels = 1200 / xAxisPointsNum;
+
+    setXAxisPointsDistance(xAxisPointsDistanceInPixels);
+
+    let xAxisPositions = [];
+
+    let start = 0;
+
+    for (let i = 0; i <= xAxisPointsNum; i++) {
+      xAxisPositions[i] = start;
+      start = start + xAxisPointsDistanceInPixels;
+    }
+
+    setXAxis(xAxisPositions);
+
+    const yAxisValues = [];
+
+    for (let i = 0; i <= 10; i++) {
+      yAxisValues[0] = Number(lowestPrice);
+      yAxisValues[i + 1] = yAxisValues[i] + Number(yAxisPointsStep);
+    }
+
+    let prettyValues = yAxisValues.map((val, _) => {
+      return val.toFixed(5);
+    });
+
+    let chartDataDateAndClosingPrice = [];
+
+    compilationArray.map((element, idx) => {
+      let convertedDate = new Date(element.date).toLocaleDateString("en-GB");
+      chartDataDateAndClosingPrice[idx] = {
+        label: convertedDate,
+        y: Number(element.close),
+      };
+    });
+
+    const xAxisValues = [];
+
+    chartDataDateAndClosingPrice.map(
+      (el, idx) =>
+        (xAxisValues[idx] = {
+          value: el.label,
+          position: idx * 10,
+        })
+    );
+
+    let yAxisPositions = [
+      0, 40, 80, 120, 160, 200, 240, 280, 320, 360, 400, 440,
+    ];
+
+    let valuesAndPointsMap = [];
+
+    for (let i = 0; i <= 11; i++) {
+      let obj = { value: prettyValues[i], pointPosition: yAxisPositions[i] };
+      valuesAndPointsMap[i] = obj;
+    }
+
+    setPretty(prettyValues);
+    setXAxisV(xAxisValues);
+    setValuesAndPoints(valuesAndPointsMap);
   };
 
   let reader;
@@ -151,171 +332,75 @@ const ForexTable = () => {
   };
   const headers = [
     "DATE",
-    "TIME",
-    "OPENING PRICE",
-    "HIGHEST PRICE",
     "LOWEST PRICE",
+    "HIGHEST PRICE",
     "CLOSING PRICE",
-    "VOLUME",
+    "OPENING PRICE",
   ];
-
-  let lowestPrice = 2;
-  let highestPrice = 0;
-  usableData.map((el, idx) => {
-    if (el.low < lowestPrice) {
-      lowestPrice = el.low;
-    }
-    if (el.high > highestPrice) {
-      highestPrice = el.high;
-    }
-  });
-
-  let lowHighDifference = highestPrice - lowestPrice;
-
-  let yAxisPointsNumber = 10;
-
-  let yAxisPointsStep = lowHighDifference / yAxisPointsNumber;
-
-  const lowHighLineHeightInPixels = (low, high) => {
-    let result = (high - low) / (lowHighDifference / 450);
-    return result;
-  };
-
-  const openCloseHeightInPixels = (open, close) => {
-    let result = (close - open) / (lowHighDifference / 450);
-    return result;
-  };
-
-  let graphElementsInPixelsArray = [];
-
-  usableData
-    .filter((el) => el.time === "18:00")
-    .map((el, idx) => {
-      graphElementsInPixelsArray[idx] = {
-        lowHighLineHeightPixels: Number(
-          lowHighLineHeightInPixels(el.low, el.high).toFixed(5)
-        ),
-        openCloseHeightPixels: Math.abs(
-          Number(openCloseHeightInPixels(el.close, el.open).toFixed(5))
-        ),
-        ...el,
-      };
-    });
-
-  console.log(graphElementsInPixelsArray);
-
-  let xAxisPointsNum = graphElementsInPixelsArray.length;
-
-  let xAxisPointsDistanceInPixels = 1200 / xAxisPointsNum;
-
-  let xAxisPositions = [];
-
-  let start = 0;
-
-  for (let i = 0; i <= xAxisPointsNum; i++) {
-    xAxisPositions[i] = start;
-    start = start + xAxisPointsDistanceInPixels;
-  }
-
-  // console.log("usable data", usableData);
-  // console.log("lowest", lowestPrice);
-  // console.log("highest", highestPrice);
-  // console.log("difference", lowHighDifference);
-  // console.log("step", yAxisPointsStep.toFixed(5));
-  console.log("a pixel corresponds to", lowHighDifference / 450);
-
-  console.log(
-    "the starting point of the y axis",
-    lowestPrice / (lowHighDifference / 450)
-  );
-
-  // let yAxisStartingPoint = lowestPrice / (lowHighDifference / 450);
-
-  // const findValuePositionInPixels = (start, value) => {
-  //   let position = value / (lowHighDifference / 450);
-  //   return position - start;
-  // };
-
-  const yAxisValues = [];
-
-  for (let i = 0; i <= 10; i++) {
-    yAxisValues[0] = Number(lowestPrice);
-    yAxisValues[i + 1] = yAxisValues[i] + Number(yAxisPointsStep);
-  }
-
-  let prettyValues = yAxisValues.map((val, _) => {
-    return val.toFixed(5);
-  });
-
-  //start herreeeeeeeee
-
-  let chartDataDateAndClosingPrice = [];
-
-  chart.map((element, idx) => {
-    chartDataDateAndClosingPrice[idx] = {
-      label: element.date,
-      y: Number(element.close),
-    };
-  });
-
-  const xAxisValues = [];
-
-  chartDataDateAndClosingPrice.map(
-    (el, idx) => (xAxisValues[idx] = { value: el.label, position: idx * 10 })
-  );
-
-  let yAxisPositions = [0, 40, 80, 120, 160, 200, 240, 280, 320, 360, 400, 440];
-
-  let valuesAndPointsMap = [];
-
-  for (let i = 0; i <= 11; i++) {
-    let obj = { value: prettyValues[i], pointPosition: yAxisPositions[i] };
-    valuesAndPointsMap[i] = obj;
-  }
 
   return (
     <Container>
-      <GraphContainer>
-        {prettyValues.map((el, idx) => (
-          <YAxisValue
-            key={idx}
-            positionb={valuesAndPointsMap[idx].pointPosition}
-          >
-            ${valuesAndPointsMap[idx].value}
-            <YAxisLine />
-          </YAxisValue>
-        ))}
-        {xAxisValues.map((el, idx) => (
-          <XAxisValue key={idx} positionl={xAxisPositions[idx]}>
-            <XAxisLine />
-            {el.value}
-          </XAxisValue>
-        ))}
-        {graphElementsInPixelsArray.map((el, idx) => (
-          <div key={idx}>
-            <LowHighLine
-              positionb={
-                el.low / (lowHighDifference / 450) -
-                lowestPrice / (lowHighDifference / 450)
-              }
-              positionl={xAxisPositions[idx] + xAxisPointsDistanceInPixels / 2}
-              lineheight={el.lowHighLineHeightPixels}
-            />
-            <OpenCloseBar
-              widthpixels={xAxisPointsDistanceInPixels}
-              positionb={
-                el.open / (lowHighDifference / 450) -
-                lowestPrice / (lowHighDifference / 450)
-              }
-              positionl={xAxisPositions[idx]}
-              barheight={
-                el.openCloseHeightPixels === 0 ? 1 : el.openCloseHeightPixels
-              }
-              color={el.open > el.close ? "red" : "lime"}
-            />
-          </div>
-        ))}
-      </GraphContainer>
+      <Graph>
+        <GraphContainer>
+          {pretty.map((el, idx) => (
+            <YAxisValue
+              key={idx}
+              positionb={valuesAndPoints[idx].pointPosition}
+            >
+              ${valuesAndPoints[idx].value}
+              <YAxisLine />
+            </YAxisValue>
+          ))}
+          {xAxisV.map((el, idx) => (
+            <XAxisValue key={idx} positionl={xAxis[idx]}>
+              <XAxisLine />
+              {el.value}
+            </XAxisValue>
+          ))}
+          {graph.map((el, idx) => (
+            <div key={idx}>
+              <LowHighLine
+                positionb={
+                  el.low / (lowHighDifferenceValue / 450) -
+                  lowest / (lowHighDifferenceValue / 450)
+                }
+                positionl={xAxis[idx] + xAxisPointsDistance / 2}
+                lineheight={el.lowHighLineHeightPixels}
+              />
+              <OpenCloseBar
+                ref={ref}
+                widthpixels={xAxisPointsDistance}
+                onMouseEnter={() => {
+                  setSelectedBar(compilation[idx]);
+                }}
+                onMouseLeave={() => {
+                  setSelectedBar();
+                }}
+                barInformation={el}
+                positionb={
+                  el.close > el.open
+                    ? el.open / (lowHighDifferenceValue / 450) -
+                      lowest / (lowHighDifferenceValue / 450)
+                    : el.close / (lowHighDifferenceValue / 450) -
+                      lowest / (lowHighDifferenceValue / 450)
+                }
+                positionl={xAxis[idx]}
+                barheight={
+                  el.openCloseHeightPixels === 0 ? 1 : el.openCloseHeightPixels
+                }
+                color={el.open > el.close ? "red" : "lime"}
+              />
+            </div>
+          ))}
+        </GraphContainer>
+        <InfoSidebar>
+          <p>Date:{selectedBar && selectedBar.date}</p>
+          <p>Low:{selectedBar && selectedBar.low}</p>
+          <p>High:{selectedBar && selectedBar.high}</p>
+          <p>Open:{selectedBar && selectedBar.open}</p>
+          <p>Close:{selectedBar && selectedBar.close}</p>
+        </InfoSidebar>
+      </Graph>
       <InputFile
         type="file"
         onChange={(e) => {
@@ -329,16 +414,14 @@ const ForexTable = () => {
             <Cell key={idx}>{item}</Cell>
           ))}
         </TableRow>
-        {usableData.length > 0 &&
-          usableData
-            .filter((el) => el.time === "18:00")
-            .map((row, id) => (
-              <TableRow key={id}>
-                {Object.keys(row).map((cellData, idx) => (
-                  <Cell key={idx}>{row[cellData]}</Cell>
-                ))}
-              </TableRow>
-            ))}
+        {compilation.length > 0 &&
+          compilation.map((row, id) => (
+            <TableRow key={id}>
+              {Object.keys(row).map((cellData, idx) => (
+                <Cell key={idx}>{row[cellData]}</Cell>
+              ))}
+            </TableRow>
+          ))}
       </Table>
     </Container>
   );
